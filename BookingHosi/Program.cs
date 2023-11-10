5598c5e7ba6d4a4277fc622f724e0eea05100416
Alan <32477198+TaoHighEn@users.noreply.github.com>
2023-11-10 09:03:40 +0800

    update

--- BookingHosi/Program.cs
+++ BookingHosi/Program.cs
@@ -1,26 +1,31 @@
 ﻿using OpenQA.Selenium; //安裝Selenium 新增
+using Select = OpenQA.Selenium.Support.UI.SelectElement;
 using OpenQA.Selenium.Chrome; //安裝Selenium Chrome新增
 using System.Net;
-using static System.Net.Mime.MediaTypeNames;
 using System.Timers;
+
 
 namespace BookingHosi
 {
     internal class Program
     {
         static bool flag = true;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Enabled = true;
-            timer.Interval = 1000;  //每分鐘
-            timer.Start();
-            timer.Elapsed += new ElapsedEventHandler(Booking);
-            if (flag)
-            {
-                Console.ReadKey();
-            }
-            
+            #region Timer
+            //System.Timers.Timer timer = new System.Timers.Timer();
+            //timer.Enabled = true;
+            //timer.Interval = 1000;  //每分鐘
+            //timer.Start();
+            //timer.Elapsed += new ElapsedEventHandler(Booking);
+            //if (flag)
+            //{
+            //    Console.ReadKey();
+            //}
+            #endregion
+            Task task = new Task(delegate { Booking(); });
+            task.Start();
+            await task;
             //Task.Run(() => {
             //    while (true)
             //    {
@@ -28,23 +33,37 @@ namespace BookingHosi
             //    }
             //});
         }
-        public static void Booking(object source, ElapsedEventArgs e)
+        public static async void Booking()
         {
-            bool f = DateTime.Now.CompareTo()
-            while ()
+            int i = DateTime.Now.CompareTo(DateTime.Parse(DateTime.Now.ToString("yyyy/MM/dd ") + "07:30:00"));
+            while (i > 0)
             {
+                flag = false;
                 Console.WriteLine("Hello, World!");
-
-                IWebDriver driver = new ChromeDriver();
+                WebDriver drivers = new ChromeDriver();
+                //IWebDriver driver = new ();
                 string str = "https://webreg.vghks.gov.tw/RSMKS/onlineReg";
-                driver.Navigate().GoToUrl(str);
+                drivers.Navigate().GoToUrl(str);
                 //隱式等待 - 直到畫面跑出資料才往下執行
-                string url = "https://webreg.vghks.gov.tw/RSMKS/api/myPost";
-                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-                httpWebRequest.Method = "POST";
-                httpWebRequest.ContentType = "application/json";
-                string json = @"";
-                //httpWebRequest.
+
+                //var elements = 
+                //var elements = driver.FindElements(By.TagName("div"));
+                var WebElements = drivers.FindElements(By.TagName("a"));
+                WebElements.First(x => x.Text == "免疫風濕科").Click();
+                drivers.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                WebElements = drivers.FindElements(By.TagName("select"));
+                Select se = new Select(WebElements.First());
+                se.SelectByValue("1");
+                var result = drivers.FindElements(By.TagName("span"));
+                //var items = result.FindElements(By.ClassName("ng-star-inserted"));
+                //var items = result.Select.FindElements(By.TagName("span"));
+                foreach (var item in result)
+                {
+                    var ii = item.GetType();
+                    Console.WriteLine(item.Text);
+                }
+                
+                //items.Click();
             }
         }
     }
